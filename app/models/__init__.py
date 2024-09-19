4a025f46b10fad3f81982d82b328030ae9214890
Joy O'Halloran <johalloran@protonmail.com>
2024-09-19 10:35:44 -0700

    renamed save_goal model file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,6 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 from .budget import Budget
-from .save_goals import SaveGoal
+from .save_goal import SaveGoal
 from .transaction import Transaction
 from .templates import Template
