9b5871b1a5be9729a84712b542b941265d32d04d
saleh-tariq <tsaleh2464@gmail.com>
2024-09-20 15:44:03 -0400

    added delete route

--- app/api/template_routes.py
+++ app/api/template_routes.py
@@ -7,6 +7,9 @@ template_routes = Blueprint("templates", __name__)
 
 @template_routes.route("/")
 def get_user_templates():
+    """
+    Get all templates made by a specific user
+    """
     user = current_user.to_dict()
     print(f"\n\n\n\n\n{user}\n\n\n\n\n")
     templates = Template.query.filter(Template.user_id == user["id"]).all()
@@ -15,5 +18,30 @@ def get_user_templates():
 
 @template_routes.route("/<int:template_id>")
 def get_template_by_id(template_id):
+    """
+    Get details of a specific template by its ID
+    """
     template = Template.query.get(template_id)
     return template.to_dict()
+
+
+@template_routes.route("/<int:template_id>", methods=["DELETE"])
+@login_required
+def delete_template(template_id):
+    template = Template.query.get(template_id)
+    if not template:
+        {"errors": {"message": "Template not found"}}, 404
+    if not template.user_id == current_user.id:
+        return {"errors": {"message": "Unauthorized"}}, 401
+
+    temp = template.to_dict()
+    db.session.delete(template)
+    db.session.commit()
+    return {"message": "Template successfully deleted", "Template": temp}
+
+
+@template_routes.route("/", methods=["post"])
+def post_new_template():
+    """
+    Create a new Template
+    """
