2f907109d791b588f9a7ca2ee82cdce935458199
saleh-tariq <tsaleh2464@gmail.com>
2024-09-20 12:57:13 -0400

    added baasic get template by id

--- app/api/template_routes.py
+++ app/api/template_routes.py
@@ -11,3 +11,8 @@ def get_user_templates():
     print(f"\n\n\n\n\n{user}\n\n\n\n\n")
     templates = Template.query.filter(Template.user_id == user["id"]).all()
     return {"Templates": [template.to_dict_simple() for template in templates]}
+
+
+@template_routes.route("/<int:template_id>")
+def get_template_by_id(template_id):
+    template = Template.query.get(template_id)
