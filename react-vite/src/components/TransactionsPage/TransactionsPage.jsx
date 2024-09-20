b2dc3868addbf82bc3e732912acd079da24641f6
Joy O'Halloran <johalloran@protonmail.com>
2024-09-20 08:17:25 -0700

    finished bare-bones /transactions frontend page

--- react-vite/src/components/TransactionsPage/TransactionsPage.jsx
+++ react-vite/src/components/TransactionsPage/TransactionsPage.jsx
@@ -1,11 +1,16 @@
 import { useLoaderData } from "react-router-dom"
+import { useEffect } from 'react'
 import TransactionSummary from "./TransactionSummary"
+import './TransactionsPage.css'
 
 export default function TransactionsPage() {
-    const { Transactions: transactions } = useLoaderData()
-    if (transactions) console.log(transactions)
+    const { Transactions } = useLoaderData()
+    useEffect(() => {
+    }, [Transactions])
+
+    if (!Transactions) return null
     return <div>
-        <h1>Your Transactions</h1>
-        {transactions.map(t => <TransactionSummary key={t.id} transaction={t} />)}
+        <h1>My Transactions</h1>
+        {Transactions.map(t => <TransactionSummary key={t.id} transaction={t} />)}
     </div>
 }
