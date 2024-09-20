a521e2af3f2ebbdf0c4886a3b593dcb60e2f4266
Joy O'Halloran <johalloran@protonmail.com>
2024-09-20 09:46:27 -0700

    added DELETE /api/transactions/:id without login verification

--- app/api/transaction_routes.py
+++ app/api/transaction_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.models import Transaction
+from app.models import Transaction, db
 from flask_login import current_user, login_required
 
 transaction_routes = Blueprint('transactions', __name__)
@@ -10,3 +10,20 @@ def get_user_transactions():
     user = current_user.to_dict()
     transactions = Transaction.query.filter(Transaction.user_id == user['id']).all()
     return {'Transactions': [transaction.to_dict() for transaction in transactions]}
+
+@transaction_routes.route('/<int:id>', methods=['DELETE'])
+# @login_required
+def delete_transaction(id):
+    # user = current_user.to_dict()
+    transaction = Transaction.query.get(id)
+    if not transaction: 
+        return {
+            'error': 'Transaction not found'
+        }, 404
+    tdict = transaction.to_dict()
+    db.session.delete(transaction)
+    db.session.commit()
+    return {
+        'message': 'Transaction successfully deleted',
+        'Transaction': tdict
+    }
