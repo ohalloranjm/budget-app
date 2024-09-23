99b1b39be819e0c768f70888149141cc91d9a6b8
Joy O'Halloran <johalloran@protonmail.com>
2024-09-23 09:38:13 -0700

    moved formatDateInternal to utils

--- react-vite/src/components/TransactionsForm/TransactionsForm.jsx
+++ react-vite/src/components/TransactionsForm/TransactionsForm.jsx
@@ -1,15 +1,7 @@
 import { useState } from "react"
 import { useLoaderData, useSubmit } from "react-router-dom"
 import toCents from "../../utils/to-cents"
-
-const formatDate = function(date) {
-    const year = date.getFullYear()
-    let month = date.getMonth() + 1
-    if (month < 10) month = '0' + month
-    let day = date.getDate()
-    if (day < 10) day = '0' + day
-    return `${year}-${month}-${day}`
-}
+import { formatDateInternal } from "../../utils/format-date"
 
 export default function TransactionsForm() {
     const submit = useSubmit()
@@ -17,7 +9,7 @@ export default function TransactionsForm() {
     const [name, setName] = useState('')
     const [amount, setAmount] = useState('')
     const [description, setDescription] = useState('')
-    const [date, setDate] = useState(formatDate(new Date()))
+    const [date, setDate] = useState(formatDateInternal(new Date()))
     const [budgetName, setBudgetName] = useState('')
 
     const { Budgets } = useLoaderData()
--- react-vite/src/utils/format-date.js
+++ react-vite/src/utils/format-date.js
@@ -5,3 +5,12 @@ export default function formatDate(dateStr) {
   const year = date.getFullYear();
   return `${month}/${day < 10 ? '0' : ''}${day}/${year}`;
 }
+
+export function formatDateInternal(date) {
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  if (month < 10) month = '0' + month;
+  let day = date.getDate();
+  if (day < 10) day = '0' + day;
+  return `${year}-${month}-${day}`;
+}
