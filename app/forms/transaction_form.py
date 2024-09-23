1e34da9c09f5b1f2d910b0b87b744be6c2dd8904
Joy O'Halloran <johalloran@protonmail.com>
2024-09-23 14:38:30 -0700

    added must-be-positive validator to transaction amount

--- app/forms/transaction_form.py
+++ app/forms/transaction_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, DateField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, NumberRange
 from app.models import Transaction
 
 class TransactionForm(FlaskForm):
-    amount = IntegerField('Amount', validators=[DataRequired()])
+    amount = IntegerField('Amount', validators=[DataRequired(), NumberRange(1, None, 'Amount must be a positive number.')])
     date = DateField('Date', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description')
