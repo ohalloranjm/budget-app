c280f60b9f62a08df53e2e04b15a5ac5d36986a1
Joy O'Halloran <johalloran@protonmail.com>
2024-10-14 13:45:52 -0700

    replaced window.confirm with separate buttons for confirm-delete functions

--- react-vite/src/components/SaveGoalDetailsPage/SaveGoalDetails.jsx
+++ react-vite/src/components/SaveGoalDetailsPage/SaveGoalDetails.jsx
@@ -1,35 +1,25 @@
-import React from "react";
-import {
-  NavLink,
-  useLoaderData,
-  useNavigate,
-  useSubmit,
-} from "react-router-dom";
-import "./SaveGoalDetails.css";
+import React, { useState } from 'react';
+import { useLoaderData, useNavigate, useSubmit } from 'react-router-dom';
+import './SaveGoalDetails.css';
 
 function SaveGoalDetails() {
   const submit = useSubmit();
   const navigate = useNavigate();
-  const saveGoal = useLoaderData() || [{ name: "uh", cost: "oh" }];
+  const saveGoal = useLoaderData() || [{ name: 'uh', cost: 'oh' }];
+  const [confirmDelete, setConfirmDelete] = useState(false);
   const start_date_num = new Date(saveGoal.start_date).getTime();
   const end_date_num = new Date(saveGoal.end_date).getTime();
   const curr_date_num = new Date().getTime();
-  const deleteSaveGoal = (e) => {
+  const deleteSaveGoal = e => {
     e.preventDefault();
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete this Save Goal?"
-    );
-    if (!confirmDelete) {
-      return;
-    }
     submit(
       { id: saveGoal.id },
-      { method: "delete", encType: "application/json" }
+      { method: 'delete', encType: 'application/json' }
     );
-    navigate("/save-goals");
+    navigate('/save-goals');
   };
   return (
-    <div className="save-goal-details">
+    <div className='save-goal-details'>
       <h2>{saveGoal.name}</h2>
       <p>{saveGoal.description}</p>
       <p>${saveGoal.cost / 100}</p>
@@ -44,16 +34,23 @@ function SaveGoalDetails() {
           value={Math.abs(start_date_num - curr_date_num)}
         ></progress>
       </span>
-      <div className="save-goal-buttons">
+      <div className='save-goal-buttons'>
         <button
-          className="dark"
+          className='dark'
           onClick={() => navigate(`/save-goals/${saveGoal.id}/edit`)}
         >
           Update
         </button>
-        <button className="dark" onClick={deleteSaveGoal}>
-          Delete
-        </button>
+
+        {confirmDelete ? (
+          <button className='dark confirm-delete' onClick={deleteSaveGoal}>
+            Confirm Delete
+          </button>
+        ) : (
+          <button className='dark' onClick={() => setConfirmDelete(true)}>
+            Delete
+          </button>
+        )}
       </div>
     </div>
   );
--- react-vite/src/components/TransactionsPage/TransactionSummary.jsx
+++ react-vite/src/components/TransactionsPage/TransactionSummary.jsx
@@ -1,26 +1,43 @@
-import { useNavigate, useSubmit } from "react-router-dom"
-import formatDate from "../../utils/format-date"
-import dollarString from "../../utils/dollar-string"
+import { useNavigate, useSubmit } from 'react-router-dom';
+import formatDate from '../../utils/format-date';
+import dollarString from '../../utils/dollar-string';
+import { useState } from 'react';
 
 export default function TransactionSummary({ transaction }) {
+  const submit = useSubmit();
+  const navigate = useNavigate();
+  const [readyToDelete, setReadyToDelete] = useState(false);
 
-    const submit = useSubmit()
-    const navigate = useNavigate()
-    
-    function deleteTransaction(e) {
-            e.preventDefault()
-            const confirmDelete = window.confirm("Are you sure you want to delete this transaction?");
-            if (!confirmDelete) return;
-            submit({id: transaction.id}, {method: 'delete', encType: 'application/json'})
-    }
-    
-    return <div className='transaction-summary-tile'>
-        <h3 className='secondary-dark'>{dollarString(transaction.amount)}</h3>
-        <p>{transaction.Budget?.name}</p>
-        <p>{transaction.name}</p>
-        <p>{formatDate(transaction.date)}</p>
-        <p>{transaction.description || '—'}</p>
-        <button className='dark' onClick={() =>navigate(`${transaction.id}/edit`)}>Edit</button>
-        <button className='dark' onClick={deleteTransaction}>Delete</button>
+  function deleteTransaction(e) {
+    e.preventDefault();
+    submit(
+      { id: transaction.id },
+      { method: 'delete', encType: 'application/json' }
+    );
+  }
+
+  return (
+    <div className='transaction-summary-tile'>
+      <h3 className='secondary-dark'>{dollarString(transaction.amount)}</h3>
+      <p>{transaction.Budget?.name}</p>
+      <p>{transaction.name}</p>
+      <p>{formatDate(transaction.date)}</p>
+      <p>{transaction.description || '—'}</p>
+      <button
+        className='dark'
+        onClick={() => navigate(`${transaction.id}/edit`)}
+      >
+        Edit
+      </button>
+      {!readyToDelete ? (
+        <button className='dark' onClick={() => setReadyToDelete(true)}>
+          Delete
+        </button>
+      ) : (
+        <button className='dark confirm-delete' onClick={deleteTransaction}>
+          Confirm Delete
+        </button>
+      )}
     </div>
+  );
 }
