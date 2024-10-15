9c8fbd5ca7da6b2080a0d46354e1c1e95ed96505
Joy O'Halloran <johalloran@protonmail.com>
2024-10-15 15:07:41 -0700

    updated seed data

--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -3,62 +3,16 @@ from datetime import datetime
 from sqlalchemy.sql import text
 
 demo_transactions = [
-    Transaction(
-        name='Walmart',
-        amount=4567,
-        date=datetime.now(),
-        user_id=1,
-        budget_id=1
-    ),
-    Transaction(
-        name='Marathon',
-        amount=5000,
-        date=datetime.now(),
-        user_id=1,
-        budget_id=2
-    ),
-    Transaction(
-        name='Robinhood',
-        amount=5,
-        date=datetime.now(),
-        user_id=1,
-        budget_id=3
-    ),
-    Transaction(
-        name='Robinhood',
-        amount=5,
-        date=datetime.now(),
-        user_id=2,
-        budget_id=1
-    ),
-    Transaction(
-        name='Personal Trainer',
-        amount=30000,
-        date=datetime(2024, 9, 1),
-        user_id=1,
-        budget_id=5
-    ),
-    Transaction(
-        name='Personal Trainer',
-        amount=30000,
-        date=datetime(2024, 10, 1),
-        user_id=1,
-        budget_id=5
-    ),
-    Transaction(
-        name='Gym membership cost',
-        amount=5000,
-        date=datetime(2024, 9, 1),
-        user_id=1,
-        budget_id=5
-    ),
-    Transaction(
-        name='Gym membership cost',
-        amount=5000,
-        date=datetime(2024, 10, 1),
-        user_id=1,
-        budget_id=5
-    ),
+    Transaction(budget_id=4, name='Gold\'s Gym', amount=3045, date=datetime(2024, 10, 2), user_id=1),
+    Transaction(budget_id=1, name='Walmart', amount=1824, date=datetime(2024, 10, 21), user_id=1),
+    Transaction(budget_id=3, name='Robinhood', amount=2252, date=datetime(2024, 10, 23), user_id=1),
+    Transaction(budget_id=3, name='Vanguard', amount=4552, date=datetime(2024, 10, 11), user_id=1),
+    Transaction(budget_id=1, name='Whole Foods', amount=3544, date=datetime(2024, 10, 22), user_id=1),
+    Transaction(budget_id=3, name='E*TRADE', amount=2107, date=datetime(2024, 10, 3), user_id=1),
+    Transaction(budget_id=3, name='E*TRADE', amount=3266, date=datetime(2024, 10, 16), user_id=1),
+    Transaction(budget_id=2, name='Shell', amount=4816, date=datetime(2024, 10, 28), user_id=1),
+    Transaction(budget_id=3, name='Vanguard', amount=1357, date=datetime(2024, 10, 2), user_id=1),
+    Transaction(budget_id=1, name='Trader Joe\'s', amount=3126, date=datetime(2024, 10, 27), user_id=1)
 ]
 
 def seed_transactions():
