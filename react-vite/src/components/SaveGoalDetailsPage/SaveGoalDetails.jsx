322b00b3d2bc376a8da742fad490e296293e581b
saleh-tariq <tsaleh2464@gmail.com>
2024-10-17 16:04:13 -0400

    dont forget to remove console.logs

--- react-vite/src/components/SaveGoalDetailsPage/SaveGoalDetails.jsx
+++ react-vite/src/components/SaveGoalDetailsPage/SaveGoalDetails.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react';
-import { useLoaderData, useNavigate, useSubmit } from 'react-router-dom';
-import './SaveGoalDetails.css';
+import { useState } from "react";
+import { useLoaderData, useNavigate, useSubmit } from "react-router-dom";
+import "./SaveGoalDetails.css";
 
 function SaveGoalDetails() {
   const submit = useSubmit();
   const navigate = useNavigate();
-  const saveGoal = useLoaderData() || [{ name: 'uh', cost: 'oh' }];
+  const saveGoal = useLoaderData() || [{ name: "uh", cost: "oh" }];
   const [confirmDelete, setConfirmDelete] = useState(false);
   const start_date_num = new Date(saveGoal.start_date).getTime();
   const end_date_num = new Date(saveGoal.end_date).getTime();
   const curr_date_num = new Date().getTime();
-  const deleteSaveGoal = e => {
+  console.log();
+  console.log();
+
+  const deleteSaveGoal = (e) => {
     e.preventDefault();
     submit(
       { id: saveGoal.id },
-      { method: 'delete', encType: 'application/json' }
+      { method: "delete", encType: "application/json" }
     );
-    navigate('/save-goals');
+    navigate("/save-goals");
   };
   return (
-    <div className='save-goal-details'>
+    <div className="save-goal-details">
       <h2>{saveGoal.name}</h2>
       <p>{saveGoal.description}</p>
       <p>${saveGoal.cost / 100}</p>
@@ -31,23 +34,23 @@ function SaveGoalDetails() {
         <p>Time Progress:</p>
         <progress
           max={end_date_num - start_date_num}
-          value={Math.abs(start_date_num - curr_date_num)}
+          value={curr_date_num - start_date_num}
         ></progress>
       </span>
-      <div className='save-goal-buttons'>
+      <div className="save-goal-buttons">
         <button
-          className='dark'
+          className="dark"
           onClick={() => navigate(`/save-goals/${saveGoal.id}/edit`)}
         >
           Update
         </button>
 
         {confirmDelete ? (
-          <button className='dark confirm-delete' onClick={deleteSaveGoal}>
+          <button className="dark confirm-delete" onClick={deleteSaveGoal}>
             Confirm Delete
           </button>
         ) : (
-          <button className='dark' onClick={() => setConfirmDelete(true)}>
+          <button className="dark" onClick={() => setConfirmDelete(true)}>
             Delete
           </button>
         )}
--- react-vite/src/components/TemplateFormPage/TemplateForm.jsx
+++ react-vite/src/components/TemplateFormPage/TemplateForm.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import { useLoaderData, useSubmit } from 'react-router-dom';
-import './TemplateForm.css';
+import { useState } from "react";
+import { useLoaderData, useSubmit } from "react-router-dom";
+import "./TemplateForm.css";
 
 function TemplateForm({ edit }) {
   const submit = useSubmit();
   const loader = useLoaderData();
   const template = loader.template;
   const Budgets = edit ? loader.budgets.Budgets : loader.Budgets;
-  const [name, setName] = useState(edit ? template.name : '');
+  const [name, setName] = useState(edit ? template.name : "");
   const [budgetsAdded, setBudgetsAdded] = useState(
     edit ? [...template.Budgets] : []
   );
@@ -15,24 +15,24 @@ function TemplateForm({ edit }) {
   //name
   const [selected, setSelected] = useState(Budgets[0]);
   const handleClick = () => {
-    if (!budgetsAdded.find(b => b.id === selected.id)) {
+    if (!budgetsAdded.find((b) => b.id === selected.id)) {
       setBudgetsAdded([...budgetsAdded, selected]);
     }
   };
 
-  const validateData = data => {
+  const validateData = (data) => {
     const validationErrors = {};
     if (!data.name) {
-      validationErrors.name = 'This field is required';
+      validationErrors.name = "This field is required";
     }
     if (data.name.length > 50) {
-      validationErrors.name = 'This field must be less than 50 chars';
+      validationErrors.name = "This field must be less than 50 chars";
     }
     console.log(Object.keys(validationErrors), validationErrors);
     return Object.keys(validationErrors).length > 0 ? validationErrors : false;
   };
 
-  const post = e => {
+  const post = (e) => {
     e.preventDefault();
     const template = {
       name,
@@ -48,11 +48,11 @@ function TemplateForm({ edit }) {
     } else
       submit(
         { template, budgets },
-        { method: 'post', encType: 'application/json' }
+        { method: "post", encType: "application/json" }
       );
   };
 
-  const put = e => {
+  const put = (e) => {
     console.log(name);
     e.preventDefault();
     const template = {
@@ -68,29 +68,33 @@ function TemplateForm({ edit }) {
     } else
       submit(
         { template, budgets },
-        { method: 'post', encType: 'application/json' }
+        { method: "post", encType: "application/json" }
       );
   };
 
   return (
-    <div className='template-form-container'>
-      <form action='post' onSubmit={edit ? put : post}>
+    <div className="template-form-container">
+      <form action="post" onSubmit={edit ? put : post}>
+        <h2 className="secondary-dark center">
+          {edit ? "Edit a Template" : "Create a New Template"}
+        </h2>
         <p>Template Name</p>
         <input
-          type='text'
-          placeholder='Template Name'
-          className='template-form-name'
-          onInput={e => setName(e.target.value)}
+          type="text"
+          placeholder="Template Name"
+          className="template-form-name"
+          onInput={(e) => setName(e.target.value)}
           value={name}
         />
-        {errors.name && <p className='alert-text'>{errors.name}</p>}
-        <div className='template-form-added-budgets'>
+        {errors.name && <p className="alert-text">{errors.name}</p>}
+        <div className="template-form-added-budgets">
           <p>Budgets</p>
           {budgetsAdded.map((ba, i) => (
-            <span key={ba.id} className='template-form-budget'>
-              <p>{ba.name + ' | ' + `$${ba.allocated}`}</p>
+            <span key={ba.id} className="template-form-budget">
+              <p>{ba.name + " | " + `$${ba.allocated}`}</p>
               <button
-                type='button'
+                type="button"
+                className="error"
                 onClick={() => {
                   console.log(
                     budgetsAdded.slice(0, i),
@@ -108,34 +112,34 @@ function TemplateForm({ edit }) {
             </span>
           ))}
         </div>
-        <div className='template-form-budgets'>
+        <div className="template-form-budgets">
           <select
-            name=''
-            id=''
-            onChange={e => {
+            name=""
+            id=""
+            onChange={(e) => {
               setSelected(
                 Budgets.find(
-                  b =>
+                  (b) =>
                     b.id === Number(e.target.options[e.target.selectedIndex].id)
                 )
               );
             }}
-            value={selected.name + ' | ' + `$${selected.allocated}`}
+            value={selected.name + " | " + `$${selected.allocated}`}
           >
-            {Budgets.map(b => {
+            {Budgets.map((b) => {
               return (
                 <option key={b.id} id={b.id}>
-                  <p>{b.name + ' | ' + `$${b.allocated}`}</p>
+                  <p>{b.name + " | " + `$${b.allocated}`}</p>
                 </option>
               );
             })}
           </select>
-          <button type='button' onClick={handleClick}>
+          <button type="button" className="dark" onClick={handleClick}>
             Add Budget
           </button>
         </div>
-        <button type='submit' className='dark'>
-          {edit ? 'Update Template' : 'Create Template'}
+        <button type="submit" className="dark">
+          {edit ? "Update Template" : "Create Template"}
         </button>
       </form>
     </div>
